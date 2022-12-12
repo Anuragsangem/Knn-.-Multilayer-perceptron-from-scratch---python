d8b3fd1344a8906c265daacaf76ea0ceff57e6ea
Sangem, Anurag <ansangem@iu.edu>
2022-12-11 19:19:43 -0500

    Update k_nearest_neighbors.py

--- k_nearest_neighbors.py
+++ k_nearest_neighbors.py
@@ -4,6 +4,32 @@
 #
 # Based on skeleton code by CSCI-B 551 Fall 2022 Course Staff
 
+"""
+#Report
+1)Abstractions : 
+a)distance_list - stores the distances of all the train set data from the test set data.
+b)neighbors - stores the k nearest neighbors in the test set 
+c)all the other absatractions are given in the initialize function in the class
+
+Ideology:
+Knn-
+a)By finding the K data points in the training set that are closest to the new data point, and using those K data points to make a prediction. 
+For example, in a binary classification task, KNN might predict that a new data point belongs to the "positive" class 
+if the majority of the K nearest neighbors belong to the "positive" class.
+b)Knn algorithm is also called as Lazy learner because there is no training that happens in the algorithm , it just calculates the distance 
+of a test set point to all the train set data points during the testing.
+
+
+Metrics : 
+a)euclidean_distance : It is calculated as the square root of the sum of the squares of the differences between the x-coordinates 
+and y-coordinates of the two points.
+b)manhattan_distance :The Manhattan distance is also known as the "taxi cab" or "rectilinear" distance, 
+because it is the distance a taxi would have to travel in a city laid out in a grid pattern to get from one point to another. 
+It is different from the Euclidean distance, which is the straight-line distance between two points
+
+
+"""
+
 
 
 #Reference : 
